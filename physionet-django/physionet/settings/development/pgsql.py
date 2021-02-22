7dd858734ae77dbc1ef440c620322964b7da8237
Subodh Jena <jenasubodh@gmail.com>
2021-02-22 11:35:44 +0530

    Deploy to heroku

--- physionet-django/physionet/settings/development/pgsql.py
+++ physionet-django/physionet/settings/development/pgsql.py
@@ -1,16 +1,13 @@
+import os
+import dj_database_url
+
 from physionet.settings.development.base import *
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'physionet',
-        'USER': 'physionet',
-        'PASSWORD': 'physionet',
-        'HOST': 'db',
-        'PORT': '',
-        'TEST': {
-            'MIRROR': 'default'
-        }
-    }
+    'default': dj_database_url.config(
+        default='postgres://physionet:physionet@db:5432/physionet',
+        conn_max_age=int(os.getenv('POSTGRES_CONN_MAX_AGE', 600))
+    )
+}
